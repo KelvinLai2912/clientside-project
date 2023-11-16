14ba31042657aad94f48417df7d59434aaedf3f7
KelvinLai2912 <kelvin.lai@live.nl>
2023-11-16 10:23:27 +0100

    Add Tailwind CSS and Flowbite, update UI components.

--- apps/clientside-indv/src/app/app.component.ts
+++ apps/clientside-indv/src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { FeaturesModule } from '@kelvin/angular-app/features';
+import { initFlowbite } from 'flowbite';
+import { UiModule } from '@kelvin/angular-app/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule, UiModule],
   selector: 'kelvin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
   title = 'clientside-indv-app';
+
+  ngOnInit(): void {
+    initFlowbite(); 
+  }
 }
